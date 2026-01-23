52636ca5cd00071b13dc0a54351ded7433e0bd44
groupfenixltda <groupfenixltda@gmail.com>
2026-01-23 12:08:43 -0300

    Upload automático via Sistema de Deploy - 2026-01-23T15:08:43.527Z

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Menu Mobile
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('fa-bars');
+    hamburger.classList.toggle('fa-times');
+});
+
+// Fechar menu ao clicar
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('fa-times');
+        hamburger.classList.add('fa-bars');
+    });
+});
+
+// Animação de Scroll (Reveal)
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
+
+// Ativar animação no carregamento
+reveal();
